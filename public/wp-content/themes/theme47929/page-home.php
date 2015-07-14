f28e1d1f7a6077b71606fb144f4c98250b44f434
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-14 17:44:32 -0300

    aa

--- public/wp-content/themes/theme47929/page-home.php
+++ public/wp-content/themes/theme47929/page-home.php
@@ -97,7 +97,7 @@ get_header(); ?>
 
 		<div class="areaitem" style="background-color: #F0F0F0;">
 			<div class="areatitulo" style="background-color: #2c3e50;">Saneamento</div>
-			<div class="areaimg" style="background-image: url('/wp-content/themes/theme47929/images/area_negocio.jpg');">&nbsp;</div>
+			<div class="areaimg" style="background-image: url('/wp-content/themes/theme47929/images/.jpg');">&nbsp;</div>
 			<div class="areatexto">
 				<div class="areatextocontent">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos</div>
 			</div>
@@ -129,7 +129,7 @@ get_header(); ?>
 
 		<div class="areaitem">
 			<div class="areatitulo" style="background-color: #e74c3c;">Ambiental</div>
-			<div class="areaimg" style="background-image: url('/wp-content/themes/theme47929/images/area_ambiente.jpg'); background-size: auto 100%;">&nbsp;</div>
+			<div class="areaimg" style="background-image: url('/wp-content/themes/theme47929/images/.jpg'); background-size: auto 100%;">&nbsp;</div>
 			<div class="areatexto">
 				<div class="areatextocontent">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos</div>
 			</div>
