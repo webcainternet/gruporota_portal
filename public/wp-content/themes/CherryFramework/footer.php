27e7aa8369ef74f7c8ec20c10e635979868d4cac
fernandofmendes <fernando.mendes@webca.com.br>
2015-07-20 17:24:35 -0300

    links

--- public/wp-content/themes/CherryFramework/footer.php
+++ public/wp-content/themes/CherryFramework/footer.php
@@ -99,7 +99,7 @@
 						<div class="footer-spacing" style="width: 27%;">
 							<ul class="footerul">
 								<li class="footerli"><div id="copyright" style="font: normal 11px/18px Open Sans !important;min-width: 300px; text-align: right;">Grupo Rota do Brasil © <?php echo date('Y'); ?><!-- [[%FOOTER_LINK]] --></div></li>
-								<li class="footerli"><div id="copyright" style="font: normal 11px/18px Open Sans !important;min-width: 300px; text-align: right;">Políticas de Privacidade | <a href="/trabalhe-conosco/">Trabalhe Conosco</a></div></li>
+								<li class="footerli"><div id="copyright" style="font: normal 11px/18px Open Sans !important;min-width: 300px; text-align: right;"><a href="/politicas-de-privacidade">Políticas de Privacidade</a> | <a href="/trabalhe-conosco">Trabalhe Conosco</a></div></li>
 							</ul>
 						</div>
 							
