dc9ba49c3cde654d0e98652ba9abe99ea81ba4bf
fernandofmendes <fernando.mendes@webca.com.br>
2015-09-18 15:37:31 -0300

    page negocios internacionais

---
+++ public/wp-content/themes/page-negocios-internacionais.php
@@ -0,0 +1,395 @@
+<?php
+/**
+* Template Name: Fullwidth Page
+*/
+
+get_header(); ?>
+
+<style type="text/css">
+.header {
+  height: 370px;
+}
+.title-header {
+	display: none;
+}
+</style>
+
+<div class="motopress-wrapper content-holder clearfix">
+	<div class="container">
+		<div class="row">
+			<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-wrapper-file="page-fullwidth.php" data-motopress-wrapper-type="content">
+				<div class="row">
+					<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-type="static" data-motopress-static-file="static/static-title.php">
+						<?php get_template_part("static/static-title"); ?>
+					</div>
+				</div>
+				<div id="content" class="row">
+					<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-type="loop" data-motopress-loop-file="loop/loop-page.php">
+						<script type="text/javascript">
+	function FechaNoticias() {
+		$('#destAC').hide();
+		$('#destAS').hide();
+		$('#destEU').hide();
+		$('#destAI').hide();
+		$('#destAF').hide();
+		$('#destOC').hide();
+	}
+</script>
+<style type="text/css">
+.wpcf7-form select, textarea, input[type="text"], input[type="password"], input[type="datetime"], input[type="datetime-local"], input[type="date"], input[type="month"], input[type="time"], input[type="week"], input[type="number"], input[type="email"], input[type="url"], input[type="search"], input[type="tel"], input[type="color"], .uneditable-input {
+width: 97%;
+}
+.title-header {
+display: none;
+}
+.content-holder {
+padding-bottom: 0px !important;
+}
+.header {
+    background-image: url('/wp-content/themes/theme47929/images/header_area_negocios.jpg');
+    background-position-y: -110px !important;
+    border-bottom: solid 5px #AB9632;
+}
+/* empresas */
+.float-empresas {
+float: left;
+width: 25%;
+text-align: center;
+margin-top: 30px;
+}
+.float-image {
+max-height: 110px;
+}
+.float-empresas-texto {
+text-align: center;
+font-size: 12px;
+padding: 15px;
+}
+/* projetos */
+.float-projetos {
+float: left;
+width: 262px;
+text-align: center;
+margin-top: 15px;
+padding: 15px;
+}
+.float-projetos-image {
+background-size: cover;
+width: 100%;
+height: 180px;
+border: solid 5px;
+}
+.float-projetos-image:hover {
+border: solid 5px #AB9632;
+}
+.float-projetos-texto {
+text-align: justify;
+font-size: 12px;
+margin-bottom: 10px;
+}
+.float-projetos-titulo {
+font-size: 14px;
+font-weight: bold;
+text-align: left;
+margin-bottom: 5px;
+}
+/* footer */
+.footer2ul a {
+color: #FFFFFF;
+}
+.footer2ul {
+margin: 0;
+padding: 0;
+list-style-type: none;
+display: inline;
+}
+.footer2li {
+display: inline;
+}
+</style>
+
+
+
+
+
+
+<div style="float: left; width: 395px; text-align: right;">
+<img src="/wp-content/uploads/logo-png/logo-grb-g.png" style="margin-right: 70px; width: 250px; margin-top: 10px;
+">
+</div>
+
+<div style="float: left; width: 765px;">
+
+<div><h2 style="color: #AB9632;">Negócios Internacionais</h2></div>
+
+<div style="text-align: justify;">Local onde fala dos 4 continentes de atuação de preve desceitivo. Segmentos mistos voltado a obras civis, eletromecânica industriais, adequação ambiental e modernização fabril assim com obras comerciais.
+
+A Rota Construtora tem como meta a conquista de novos espaços no mercado de construção nacional e internacional em suas áreas de atuação, visando assegurar através de suas competências, a confiança de seus clientes, o que é por nós considerado característica primordial para transformar um projeto em realidade sendo creditado aos nossos clientes internos e externos o sucesso por nós alcançados.</div>
+
+
+</div>
+
+
+<style type="text/css">
+	.abacontinente {
+		float: left; margin-left: 5px; margin-right: 5px; color: #AB9632; padding: 1px 10px; font-size: 16px;
+	}
+	.abacontinente:hover {
+		color: #FFFFFF;
+		background-color: #AB9632;
+	}
+.abacontinentesel {
+	float: left; background-color: #AB9632; color: #FFFFFF; padding: 1px 10px; font-size: 16px;
+	cursor: pointer;
+}
+.abalinksel {
+	color: #FFFFFF;
+}
+.abalink {
+	color: #333;
+}
+</style>
+<div style="float: left; font: normal 22px Open Sans; margin-top: 0px; width: 100%; padding: 10px 10px 0px;border-bottom: solid 2px #AB9632;color: #AB9632; max-width: 1150px; margin-bottom: 15px; margin-top: 30px;">
+
+
+<div class="abacontinentesel"><a class="abalinksel" onclick="javascript: FechaNoticias(); $('#destAC').show();">América do Norte</a></div>
+<div class="abacontinente"><a class="abalink" onclick="javascript: FechaNoticias(); $('#destAC').show();">América Central</a></div>
+<div class="abacontinente"><a class="abalink" onclick="javascript: FechaNoticias(); $('#destAS').show();">América do Sul</a></div>
+<div class="abacontinente"><a class="abalink" onclick="javascript: FechaNoticias(); $('#destEU').show();">Europa</a></div>
+<div class="abacontinente"><a class="abalink" onclick="javascript: FechaNoticias(); $('#destAI').show();">Asia</a></div>
+<div class="abacontinente"><a class="abalink" onclick="javascript: FechaNoticias(); $('#destAF').show();">Africa</a></div>
+<div class="abacontinente"><a class="abalink" onclick="javascript: FechaNoticias(); $('#destOC').show();">Oceania</a></div>
+
+</div> 
+
+
+<!-- América Central -->
+<div id="destAC" style="display: none;">
+<div style="float: left; width: 800px;margin-top: -25px;">
+
+<div style="float: left; font: normal 22px Open Sans; margin-top: 30px; width:770px; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632;margin-bottom: 15px;">Destaques: América Central</div>
+
+<div class="novoticket" style="float: left;  width: 100%;">
+[metaslider id=2601]
+</div>
+</div>
+
+<div style="float: left; width: 340px; margin-left: 30px;margin-top: -25px;">
+<div style="float: left; font: normal 22px Open Sans; margin-top: 30px; width:310px; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632;margin-bottom: 15px;">Últimas Notícias</div>
+
+<div class="novoticket" style="float: left;  width: 100%;">
+<a class="noticiaslinks" href="">Pesquisa traça perfil de vítimas de violência doméstica (Diário de Pernambuco – 28/08/2015)</a>
+<a class="noticiaslinks" href="">Lei específica não será efetiva sem mudança na mentalidade social (Blasting News – 28/08/2015)</a>
+<a class="noticiaslinks" href="">Juíza defende a criação de banco de dados de homicídios de mulheres (Jornal do Brasil – 28/08/2015)</a>
+<a class="noticiaslinks" href="">Leis Maria da Penha e do Feminicídio em pauta em seminário nesta sexta (Diário de Pernambuco – 28/08/2015)</a>
+<a class="noticiaslinks" href="">Lei do Feminicídio encara a misoginia (Gazeta do Povo – 28/08/2015)</a>
+</div>
+</div>
+</div>
+<!-- FIM - América Central -->
+
+
+
+<!-- América do Sul -->
+<div id="destAS" style="display: none;">
+<div style="float: left; width: 800px;margin-top: -25px;">
+
+<div style="float: left; font: normal 22px Open Sans; margin-top: 30px; width:770px; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632;margin-bottom: 15px;">Destaques: América Central</div>
+
+<div class="novoticket" style="float: left;  width: 100%;">
+[metaslider id=2601]
+</div>
+</div>
+
+<div style="float: left; width: 340px; margin-left: 30px;margin-top: -25px;">
+<div style="float: left; font: normal 22px Open Sans; margin-top: 30px; width:310px; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632;margin-bottom: 15px;">Últimas Notícias</div>
+
+<div class="novoticket" style="float: left;  width: 100%;">
+<a class="noticiaslinks" href="">Pesquisa traça perfil de vítimas de violência doméstica (Diário de Pernambuco – 28/08/2015)</a>
+<a class="noticiaslinks" href="">Lei específica não será efetiva sem mudança na mentalidade social (Blasting News – 28/08/2015)</a>
+<a class="noticiaslinks" href="">Juíza defende a criação de banco de dados de homicídios de mulheres (Jornal do Brasil – 28/08/2015)</a>
+<a class="noticiaslinks" href="">Leis Maria da Penha e do Feminicídio em pauta em seminário nesta sexta (Diário de Pernambuco – 28/08/2015)</a>
+<a class="noticiaslinks" href="">Lei do Feminicídio encara a misoginia (Gazeta do Povo – 28/08/2015)</a>
+</div>
+</div>
+</div>
+<!-- FIM - América do Sul -->
+
+
+
+
+
+
+<div style="float: left;">
+<table style="width: 100%;">
+<tr>
+
+<td style="width: 33.33%;vertical-align: top;">
+<div style="float: left; font: normal 22px Open Sans; margin-top: 0px; width: 100%; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632; max-width: 355px; margin-bottom: 15px;">Câmbio</div>
+<div style="float: left; width: 385px;">
+<iframe src="http://gruporota2.homolog.lojavirtual.digital/wp-content/themes/theme47929/cotacao.php" width="385" border="0" scrolling="no" noresize style="height: 460px; border: 0px;"></iframe>
+</div>
+</td>
+
+<td style="width: 33.33%;vertical-align: top; padding-left: 30px;">
+<div style="float: left; font: normal 22px Open Sans; margin-top: 0px; width: 100%; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632; max-width: 350px; margin-bottom: 15px;">Conversores</div>
+
+<div style="float: left; width: 385px;">
+<iframe src="http://gruporota2.homolog.lojavirtual.digital/wp-content/themes/theme47929/conversor.php" width="385" border="0" scrolling="no" noresize style="height: 310px; border: 0px;"></iframe>
+</div>
+</td>
+
+<td style="width: 33.33%;vertical-align: top; padding-left: 25px;">
+<div style="float: left; font: normal 22px Open Sans; margin-top: 0px; width:339px; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632; max-width: 350px; margin-bottom: 15px;">Indicadores</div><img src="/wp-content/themes/theme47929/images/indicadores.png">
+</td>
+</tr>
+</table>
+</div>
+
+
+
+
+
+
+
+<div style="float: left; font: normal 22px Open Sans; margin-top: 30px; width: 100%; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632; max-width: 1140px; margin-bottom: 5px;">Áreas de Atuação</div>
+
+
+
+
+<div style="float: left; width: 50%; margin: 0px; padding: 0px;background-color: #f1f1f1;margin-bottom: 5px;">
+<div style="float: left; width: 250px; margin-right: 10px; background-image: url('http://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2014/06/13/andressanchezgetty.jpg'); background-size: cover; height: 180px;"></div>
+<div style="float: left; width: 290px;">
+<div style="font-size: 18px; margin-top: 10px; margin-bottom: 10px; color: #2c3e50;">Notícia lorem Ipsum é simplesmente uma simulação</div>
+<div style="text-align: justify;">Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</div>
+</div>
+</div>
+
+<div style="float: left; width: 50%; margin: 0px; padding: 0px;background-color: #f1f1f1;margin-bottom: 5px;">
+<div style="float: left; width: 250px; margin-right: 10px; background-image: url('http://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2014/06/13/andressanchezgetty.jpg'); background-size: cover; height: 180px;"></div>
+<div style="float: left; width: 290px;">
+<div style="font-size: 18px; margin-top: 10px; margin-bottom: 10px; color: #2c3e50;">Notícia lorem Ipsum é simplesmente uma simulação</div>
+<div style="text-align: justify;">Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</div>
+</div>
+</div>
+
+<div style="float: left; width: 50%; margin: 0px; padding: 0px;background-color: #f1f1f1;margin-bottom: 5px;">
+<div style="float: left; width: 250px; margin-right: 10px; background-image: url('http://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2014/06/13/andressanchezgetty.jpg'); background-size: cover; height: 180px;"></div>
+<div style="float: left; width: 290px;">
+<div style="font-size: 18px; margin-top: 10px; margin-bottom: 10px; color: #2c3e50;">Notícia lorem Ipsum é simplesmente uma simulação</div>
+<div style="text-align: justify;">Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</div>
+</div>
+</div>
+
+<div style="float: left; width: 50%; margin: 0px; padding: 0px;background-color: #f1f1f1;margin-bottom: 5px;">
+<div style="float: left; width: 250px; margin-right: 10px; background-image: url('http://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2014/06/13/andressanchezgetty.jpg'); background-size: cover; height: 180px;"></div>
+<div style="float: left; width: 290px;">
+<div style="font-size: 18px; margin-top: 10px; margin-bottom: 10px; color: #2c3e50;">Notícia lorem Ipsum é simplesmente uma simulação</div>
+<div style="text-align: justify;">Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</div>
+</div>
+</div>
+
+<div style="float: left; width: 50%; margin: 0px; padding: 0px;background-color: #f1f1f1;margin-bottom: 5px;">
+<div style="float: left; width: 250px; margin-right: 10px; background-image: url('http://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2014/06/13/andressanchezgetty.jpg'); background-size: cover; height: 180px;"></div>
+<div style="float: left; width: 290px;">
+<div style="font-size: 18px; margin-top: 10px; margin-bottom: 10px; color: #2c3e50;">Notícia lorem Ipsum é simplesmente uma simulação</div>
+<div style="text-align: justify;">Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</div>
+</div>
+</div>
+
+<div style="float: left; width: 50%; margin: 0px; padding: 0px;background-color: #f1f1f1;margin-bottom: 5px;">
+<div style="float: left; width: 250px; margin-right: 10px; background-image: url('http://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2014/06/13/andressanchezgetty.jpg'); background-size: cover; height: 180px;"></div>
+<div style="float: left; width: 290px;">
+<div style="font-size: 18px; margin-top: 10px; margin-bottom: 10px; color: #2c3e50;">Notícia lorem Ipsum é simplesmente uma simulação</div>
+<div style="text-align: justify;">Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<table style="width: 100%">
+<tr>
+<td style="width: 50%; vertical-align: top;">
+
+<div style="float: left; font: normal 22px Open Sans; width: 100%; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632; max-width: 553px; margin-bottom: 5px; margin-top: 22px;">Titulo desta area</div>
+
+
+<div style="float: left; width: 100%; margin: 0px; padding: 0px; margin-top: 10px; text-align: justify; margin-bottom: 5px;">
+Ainda não temos o conteúdo desta área. É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência similar a de um texto legível. Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito (injetando humor, e coisas do gênero).
+</div>
+
+</td>
+
+<td style="width: 50%; vertical-align: top;">
+<div style="padding-left: 15px;">
+<div>
+<h2 style="color: #AB9632;">
+<div style="background-color: #AB9632;position: absolute; font: normal 15px/20px Open Sans !important; z-index: 10; padding: 8px 15px 8px 15px; text-transform: uppercase; color: #FFFFFF;">Insira sua demanda</div>
+</h2>
+</div>
+<div style="padding-top: 50px;">
+[contact-form-7 id="2664" title="Contato Internacional"]
+</div>
+</div>
+</td>
+
+</tr>
+</table>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</div></div></div>
+
+<!-- Rodape interno -->
+<a href="/fale-conosco/">
+<div style="background-image: url('/wp-content/themes/theme47929/images/header_bg_faleconosco_all.jpg');height: 200px;  background-position-y: -175px;float: left; margin-top: 40px;width: 1230px; ">
+  <div class="camera_caption fadeIn" style="position: relative; opacity: 0.8;"><div style="padding: 20px;margin: 20px;width: 115px;font-size: 16px;color: #FFFFFF;background-color: #AB9632;text-transform: uppercase;">
+                Fale conosco
+    </div></div>
+</div>
+</a>
+
+<div style="background-color: #AB9632; float: left; width: 1170px; padding: 30px; text-align: center;">
+<ul class="footer2ul">
+<li class="footer2li"><a href="/engenharia-e-construcao">Engenharia e construção</a> | </li>
+<li class="footer2li"><a href="/servicos-empresariais">Serviços empresariais</a> | </li>
+<li class="footer2li"><a href="/seguranca-patrimonial">Segurança Patrimonial</a> | </li>
+<li class="footer2li"><a href="/imobiliario">Imobiliário</a> | </li>
+<li class="footer2li"><a href="/saneamento">Saneamento</a> | </li>
+<li class="footer2li"><a href="/transporte-e-logistica">Transporte e Logística</a> | </li>
+<li class="footer2li"><u><b><a href="/negocios-internacionais">Negocios Internacionais</a></b></u> | </li>
+<li class="footer2li"><a href="/responsabilidade-social">Responsabilidade Social</a> | </li>
+<li class="footer2li"><a href="/ambiental">Ambiental</a></li>
+</ul>
+</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
