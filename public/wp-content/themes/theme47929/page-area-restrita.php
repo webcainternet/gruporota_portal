258b91ff9b4592aab641f3781d5400647b8ad88a
fernandofmendes <fernando.mendes@webca.com.br>
2015-09-22 06:43:31 -0300

    design login

--- public/wp-content/themes/theme47929/page-area-restrita.php
+++ public/wp-content/themes/theme47929/page-area-restrita.php
@@ -6,42 +6,50 @@
 get_header(); ?>
 
 <script type="text/javascript">
-	function LimpaLable() {
-		$('#lbcliente').hide();
-		$('#lbconselho').hide();
-		$('#lbwebmail').hide();
-	}
-
-	function btCliente() {
-		LimpaLable();
-	    $('#lbcliente').show();
-	    btVoltar()
-	}
-
-	function btConselho() {
-		LimpaLable();
-	    $('#lbconselho').show();
-	    btVoltar()
-	}
-
-	function btWebmail() {
-		LimpaLable();
-	    $('#lbwebmail').show();
-	    btVoltar()
-	}
-
-	function btLogin() {
-		$("#dvLogin").fadeOut(1000);
-	    $("#dvSenha").delay(1000).fadeIn(1000);
-	}
-
 	function btVoltar() {
-		$("#dvSenha").fadeOut(1000);
-	    $("#dvLogin").delay(1000).fadeIn(1000);
+		$('#area-colaboradores').fadeOut(1000);
+		$('#area-cliente').fadeOut(1000);
+		$('#area-conselho').fadeOut(1000);
+		$('#area-webmail').fadeOut(1000);
+		$('#area-home').delay(1015).fadeIn(1000);
+	}
+	function MostraColaboradores() {
+		$('#area-home').fadeOut(1000);
+		$('#area-colaboradores').delay(1015).fadeIn(1000);
+	}
+	function MostraConselho() {
+		$('#area-home').fadeOut(1000);
+		$('#area-conselho').delay(1015).fadeIn(1000);
+	}
+	function MostraWebmail() {
+		$('#area-home').fadeOut(1000);
+		$('#area-webmail').delay(1015).fadeIn(1000);
+	}
+	function MostraCliente() {
+		$('#area-home').fadeOut(1000);
+		$('#area-cliente').delay(1015).fadeIn(1000);
 	}
 </script>
 
 <style type="text/css">
+
+
+#area-colaboradores {
+	display: none;
+}
+
+#area-cliente {
+	display: none;
+}
+
+#area-conselho {
+	display: none;
+}
+
+#area-webmail {
+	display: none;
+}
+
 .header {
   height: 370px;
 }
@@ -50,23 +58,30 @@ get_header(); ?>
 }
 .BoxPortal {
 	color: #FFFFFF;
-	background-color: #0097A9;
+	background-color: #304C67;
 	float: left;
 	width: 280px;
 	height: 200px;
-	display: none;
 }
 .BoxFaleConosco {
-	color: #333333;
-	background-color: #FFFFFF;
+	color: #FFFFFF;
+	background-color: #eea303;
 	float: left;
-	width: 140px;
+	width: 280px;
+	height: 200px;
+	margin-left: 5px;
+}
+.BoxCliente {
+	color: #FFFFFF;
+	background-color: #2c3e50; /* #982B00; vinho */
+	float: left;
+	width: 280px;
 	height: 200px;
 	margin-left: 5px;
 }
 .BoxConselho {
 	color: #FFFFFF;
-	background-color: #2c3e50; /* #982B00; vinho */
+	background-color: #222222; /* #982B00; vinho */
 	float: left;
 	width: 280px;
 	height: 200px;
@@ -127,6 +142,12 @@ get_header(); ?>
 .coreea303:hover {
 	color: #eea303 !important;
 }
+.cor222:hover {
+	color: #222 !important;
+}
+.cor304C67:hover {
+	color: #304C67 !important;
+}
 </style>
 
 <link rel="stylesheet" type="text/css" href="/wp-content/plugins/IconHoverEffects/css/default.css" />
@@ -152,81 +173,119 @@ get_header(); ?>
 						</style>
 
 
-						<div style="float: left; width: 535px;">
-							<h2 style="margin-bottom: 45px;">Acesso para clientes</h2>
+						<div id="area-home" style="float: left; width: 100%;">
+							<h2 style="margin-bottom: 45px;">Acesso Restrito</h2>
 
-							<div class="BoxPortal">
+							<div class="BoxPortal" onclick="javascript: MostraColaboradores();">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
-									<a href="#set-5" class="hi-icon hi-icon-earth">Portal</a>
-									<div>Ir para Website</div>
+									<a href="#set-5" class="hi-icon hi-icon-earth cor304C67">Portal</a>
+									<div>Acesso Colaboradores</div>
 								</div>
 							</div>
-							
-							<div class="BoxFaleConosco" onclick="javascript: btCliente();">
-								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a" style="padding: 0em 0 3em;">
-									<a href="#set-5" class="hi-icon hi-icon-user" style="color: #333333; box-shadow: 0 0 0 4px #333333;">Portal</a>
+
+							<div class="BoxCliente" onclick="javascript: MostraCliente();">
+								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
+									<a href="#set-5" class="hi-icon hi-icon-user cor2c3e50">Portal</a>
 									<div>Acesso Cliente</div>
 								</div>
 							</div>
 
-							<div class="BoxDolar">
+							<div class="BoxEmail" onclick="javascript: MostraWebmail();">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
-									<a href="#set-5" class="hi-icon hi-icon-user">Portal</a>
-									<div style="display: none;">Acesso Restrito</div>
+									<a href="#set-5" class="hi-icon hi-icon-mail coreea303">Portal</a>
+									<div>Webmail</div>
+								</div>
+							</div>
+							
+							<div class="BoxConselho" onclick="javascript: MostraConselho();">
+								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
+									<a href="#set-5" class="hi-icon hi-icon-locked cor222">Portal</a>
+									<div>Conselho de Administração</div>
+								</div>
+							</div>
+						</div>
+
+						<div id="area-colaboradores" style="float: left; width: 100%;">
+							<h2 style="margin-bottom: 45px;">Acesso Colaboradores</h2>
+
+							<div class="BoxPortal" style="margin-left: 285px;">
+								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
+									<a href="#set-5" class="hi-icon hi-icon-earth cor304C67">Portal</a>
+									<div>Acesso Colaboradores</div>
 								</div>
 							</div>
 
 							<div id="dvLogin" style="float: left; width: 285px; margin-left: 15px;">
-								<h2 style="font-size: 16px; margin-bottom: 0px;">Login <span id="lbcliente" style="padding: 0px 5px; color: #FFFFFF; background-color: #333;">Cliente</span><span id="lbconselho" style="padding: 0px 5px; color: #FFFFFF; background-color: #2c3e50; display: none;">Conselho de Administração</span><span id="lbwebmail" style="padding: 0px 5px; color: #FFFFFF; background-color: #eea303; display: none;">Webmail</span></h2>
+								<h2 style="font-size: 16px; margin-bottom: 0px;">Login</h2>
 								<div><input type="text" style="width: 315px; height: 50px;" placeholder="Digite seu login"></div>
-								<div style="text-align: right; width: 315px;"><input onclick="javascript: btLogin();" type="button" value="Entrar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px;"></div>
-							</div>
-							
-							<div id="dvSenha" style="float: left; width: 285px; margin-left: 15px; display: none;">
+								
 								<h2 style="font-size: 16px; margin-bottom: 0px;">Senha</h2>
 								<div><input type="password" style="width: 315px; height: 50px;" placeholder="Digite sua senha"></div>
 								<div style="text-align: right; width: 315px;"><input onclick="javascript: btVoltar();" type="button" value="Voltar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px; margin-right: 5px;"><input type="button" value="Entrar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px;"></div>
 							</div>
 						</div>
 
-						<div style="float: left; width: 50%; padding-left: 15px;">
-							<h2>&nbsp;</h2>
-							
-							<div class="BoxConselho" onclick="javascript: btConselho();">
+						<div id="area-cliente" style="float: left; width: 100%;">
+							<h2 style="margin-bottom: 45px;">Acesso Cliente</h2>
+
+							<div class="BoxCliente" onclick="javascript: MostraCliente();" style="margin-left: 285px;">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
-									<a href="#set-5" class="hi-icon hi-icon-locked cor2c3e50">Portal</a>
-									<div>Conselho de Administração</div>
+									<a href="#set-5" class="hi-icon hi-icon-user cor2c3e50">Portal</a>
+									<div>Acesso Cliente</div>
 								</div>
 							</div>
 
-							<div class="BoxSerido">
-								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
-									<a href="#set-5" class="hi-icon hi-icon-list">Portal</a>
-									<div>Seridó Group Participations</div>
-								</div>
+							<div id="dvLogin" style="float: left; width: 285px; margin-left: 15px;">
+								<h2 style="font-size: 16px; margin-bottom: 0px;">Login</h2>
+								<div><input type="text" style="width: 315px; height: 50px;" placeholder="Digite seu login"></div>
+								
+								<h2 style="font-size: 16px; margin-bottom: 0px;">Senha</h2>
+								<div><input type="password" style="width: 315px; height: 50px;" placeholder="Digite sua senha"></div>
+								<div style="text-align: right; width: 315px;"><input onclick="javascript: btVoltar();" type="button" value="Voltar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px; margin-right: 5px;"><input type="button" value="Entrar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px;"></div>
 							</div>
+						</div>
 
-							<div class="BoxEmail" onclick="javascript: btWebmail();">
+						<div id="area-webmail" style="float: left; width: 100%;">
+							<h2 style="margin-bottom: 45px;">Webmail</h2>
+
+							<div class="BoxEmail" onclick="javascript: btWebmail();" style="margin-left: 285px;">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
 									<a href="#set-5" class="hi-icon hi-icon-mail coreea303">Portal</a>
 									<div>Webmail</div>
 								</div>
 							</div>
 
-							<div class="BoxWebAdmin">
+							<div id="dvLogin" style="float: left; width: 285px; margin-left: 15px;">
+								<h2 style="font-size: 16px; margin-bottom: 0px;">Login</h2>
+								<div><input type="text" style="width: 315px; height: 50px;" placeholder="Digite seu login"></div>
+								
+								<h2 style="font-size: 16px; margin-bottom: 0px;">Senha</h2>
+								<div><input type="password" style="width: 315px; height: 50px;" placeholder="Digite sua senha"></div>
+								<div style="text-align: right; width: 315px;"><input onclick="javascript: btVoltar();" type="button" value="Voltar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px; margin-right: 5px;"><input type="button" value="Entrar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px;"></div>
+							</div>
+						</div>
+
+						<div id="area-conselho" style="float: left; width: 100%;">
+							<h2 style="margin-bottom: 45px;">Conselho de Administração</h2>
+
+							<div class="BoxConselho" onclick="javascript: btConselho();" style="margin-left: 285px;">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
-									<!-- <a href="#set-5" class="hi-icon hi-icon-cog">Portal</a> -->
-									<div>Site Admin</div>
+									<a href="#set-5" class="hi-icon hi-icon-locked cor222">Portal</a>
+									<div>Conselho de Administração</div>
 								</div>
 							</div>
 
-							<div class="BoxSharePoint">
-								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
-									<a href="#set-5" class="hi-icon hi-icon-list">Portal</a>
-									<div>Share Point</div>
-								</div>
+							<div id="dvLogin" style="float: left; width: 285px; margin-left: 15px;">
+								<h2 style="font-size: 16px; margin-bottom: 0px;">Login</h2>
+								<div><input type="text" style="width: 315px; height: 50px;" placeholder="Digite seu login"></div>
+								
+								<h2 style="font-size: 16px; margin-bottom: 0px;">Senha</h2>
+								<div><input type="password" style="width: 315px; height: 50px;" placeholder="Digite sua senha"></div>
+								<div style="text-align: right; width: 315px;"><input onclick="javascript: btVoltar();" type="button" value="Voltar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px; margin-right: 5px;"><input type="button" value="Entrar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px;"></div>
 							</div>
 						</div>
+
+
 
 						<div style="float: left; margin-top: 40px; width: 100%; border-top: dashed 1px #2c3e50; margin-left: 5px; padding-top: 10px;">
 							<h2 style="font-size: 16px; margin-bottom: 0px;">Problemas com o login?</h2>
