2a32f74974443a9f6b03f66411c44d22ebdfff5e
fernandofmendes <fernando.mendes@webca.com.br>
2015-09-23 14:52:40 -0300

    area restrita com investidor

--- public/wp-content/themes/theme47929/page-area-restrita.php
+++ public/wp-content/themes/theme47929/page-area-restrita.php
@@ -10,6 +10,7 @@ get_header(); ?>
 		$('#area-colaboradores').fadeOut(1000);
 		$('#area-cliente').fadeOut(1000);
 		$('#area-conselho').fadeOut(1000);
+		$('#area-investidor').fadeOut(1000);
 		$('#area-webmail').fadeOut(1000);
 		$('#area-home').delay(1015).fadeIn(1000);
 	}
@@ -29,6 +30,10 @@ get_header(); ?>
 		$('#area-home').fadeOut(1000);
 		$('#area-cliente').delay(1015).fadeIn(1000);
 	}
+	function MostraInvestidor() {
+		$('#area-home').fadeOut(1000);
+		$('#area-investidor').delay(1015).fadeIn(1000);
+	}
 </script>
 
 <style type="text/css">
@@ -39,6 +44,10 @@ get_header(); ?>
 }
 
 #area-cliente {
+	display: none;
+}
+
+#area-investidor {
 	display: none;
 }
 
@@ -56,84 +65,49 @@ get_header(); ?>
 .title-header {
 	display: none;
 }
+
+.BoxTodosMaior {
+	width: 280px;
+	height: 200px;
+}
+
+.BoxTodos {
+	width: 230px;
+	height: 200px;
+}
+
 .BoxPortal {
 	color: #FFFFFF;
 	background-color: #304C67;
 	float: left;
-	width: 280px;
-	height: 200px;
 }
-.BoxFaleConosco {
-	color: #FFFFFF;
-	background-color: #eea303;
-	float: left;
-	width: 280px;
-	height: 200px;
-	margin-left: 5px;
-}
+
 .BoxCliente {
 	color: #FFFFFF;
 	background-color: #2c3e50; /* #982B00; vinho */
 	float: left;
-	width: 280px;
-	height: 200px;
 	margin-left: 5px;
 }
+
+.BoxInvestidor {
+	color: #FFFFFF;
+	background-color: #1C2C3C; /* #982B00; vinho */
+	float: left;
+	margin-left: 5px;
+}
+
 .BoxConselho {
 	color: #FFFFFF;
 	background-color: #222222; /* #982B00; vinho */
 	float: left;
-	width: 280px;
-	height: 200px;
 	margin-left: 5px;
 }
+
 .BoxEmail {
 	color: #FFFFFF;
 	background-color: #eea303;
 	float: left;
-	width: 280px;
-	height: 200px;
 	margin-left: 5px;
-}
-.BoxSerido {
-	color: #FFFFFF;
-	background-color: #009D00; /* 2C89EF */
-	float: left;
-	width: 280px;
-	height: 200px;
-	margin-left: 5px;
-	display: none;
-}
-.BoxWebAdmin {
-	color: #FFFFFF;
-    background-color: #312720;
-    float: left;
-    width: 137px;
-    height: 110px;
-    margin-left: 5px;
-    margin-top: 5px;
-    padding-top: 35px;
-    display: none;
-}
-
-.BoxSharePoint {
-	color: #FFFFFF;
-	background-color: #6F86B4;
-	float: left;
-	width: 137px;
-	height: 200px;
-	margin-left: 5px;
-	margin-top: 5px;
-	display: none;
-}
-.BoxDolar {
-	color: #FFFFFF;
-	background-color: #D8522C;
-	float: left;
-	width: 565px;
-	height: 100px;
-	margin-top: 5px;
-	display: none;
 }
 
 .cor2c3e50:hover {
@@ -176,28 +150,35 @@ get_header(); ?>
 						<div id="area-home" style="float: left; width: 100%;">
 							<h2 style="margin-bottom: 45px;">Acesso Restrito</h2>
 
-							<div class="BoxPortal" onclick="javascript: MostraColaboradores();">
+							<div class="BoxTodos BoxPortal" onclick="javascript: MostraColaboradores();">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
 									<a href="#set-5" class="hi-icon hi-icon-earth cor304C67">Portal</a>
-									<div>Acesso Colaboradores</div>
+									<div>Acesso Colaborador</div>
 								</div>
 							</div>
 
-							<div class="BoxCliente" onclick="javascript: MostraCliente();">
+							<div class="BoxTodos BoxCliente" onclick="javascript: MostraCliente();">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
 									<a href="#set-5" class="hi-icon hi-icon-user cor2c3e50">Portal</a>
 									<div>Acesso Cliente</div>
 								</div>
 							</div>
 
-							<div class="BoxEmail" onclick="javascript: MostraWebmail();">
+							<div class="BoxTodos BoxInvestidor" onclick="javascript: MostraInvestidor();">
+								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
+									<a href="#set-5" class="hi-icon hi-icon-archive cor2c3e50">Portal</a>
+									<div>Acesso Investidor</div>
+								</div>
+							</div>
+
+							<div class="BoxTodos BoxEmail" onclick="javascript: MostraWebmail();">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
 									<a href="#set-5" class="hi-icon hi-icon-mail coreea303">Portal</a>
 									<div>Webmail</div>
 								</div>
 							</div>
 							
-							<div class="BoxConselho" onclick="javascript: MostraConselho();">
+							<div class="BoxTodos BoxConselho" onclick="javascript: MostraConselho();">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
 									<a href="#set-5" class="hi-icon hi-icon-locked cor222">Portal</a>
 									<div>Conselho de Administração</div>
@@ -208,10 +189,10 @@ get_header(); ?>
 						<div id="area-colaboradores" style="float: left; width: 100%;">
 							<h2 style="margin-bottom: 45px;">Acesso Colaboradores</h2>
 
-							<div class="BoxPortal" style="margin-left: 285px;">
+							<div class="BoxTodosMaior BoxPortal" style="margin-left: 285px;">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
 									<a href="#set-5" class="hi-icon hi-icon-earth cor304C67">Portal</a>
-									<div>Acesso Colaboradores</div>
+									<div>Acesso Colaborador</div>
 								</div>
 							</div>
 
@@ -228,7 +209,7 @@ get_header(); ?>
 						<div id="area-cliente" style="float: left; width: 100%;">
 							<h2 style="margin-bottom: 45px;">Acesso Cliente</h2>
 
-							<div class="BoxCliente" onclick="javascript: MostraCliente();" style="margin-left: 285px;">
+							<div class="BoxTodosMaior BoxCliente" onclick="javascript: MostraCliente();" style="margin-left: 285px;">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
 									<a href="#set-5" class="hi-icon hi-icon-user cor2c3e50">Portal</a>
 									<div>Acesso Cliente</div>
@@ -248,7 +229,7 @@ get_header(); ?>
 						<div id="area-webmail" style="float: left; width: 100%;">
 							<h2 style="margin-bottom: 45px;">Webmail</h2>
 
-							<div class="BoxEmail" onclick="javascript: btWebmail();" style="margin-left: 285px;">
+							<div class="BoxTodosMaior BoxEmail" onclick="javascript: btWebmail();" style="margin-left: 285px;">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
 									<a href="#set-5" class="hi-icon hi-icon-mail coreea303">Portal</a>
 									<div>Webmail</div>
@@ -268,10 +249,30 @@ get_header(); ?>
 						<div id="area-conselho" style="float: left; width: 100%;">
 							<h2 style="margin-bottom: 45px;">Conselho de Administração</h2>
 
-							<div class="BoxConselho" onclick="javascript: btConselho();" style="margin-left: 285px;">
+							<div class="BoxTodosMaior BoxConselho" onclick="javascript: btConselho();" style="margin-left: 285px;">
 								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
 									<a href="#set-5" class="hi-icon hi-icon-locked cor222">Portal</a>
 									<div>Conselho de Administração</div>
+								</div>
+							</div>
+
+							<div id="dvLogin" style="float: left; width: 285px; margin-left: 15px;">
+								<h2 style="font-size: 16px; margin-bottom: 0px;">Login</h2>
+								<div><input type="text" style="width: 315px; height: 50px;" placeholder="Digite seu login"></div>
+								
+								<h2 style="font-size: 16px; margin-bottom: 0px;">Senha</h2>
+								<div><input type="password" style="width: 315px; height: 50px;" placeholder="Digite sua senha"></div>
+								<div style="text-align: right; width: 315px;"><input onclick="javascript: btVoltar();" type="button" value="Voltar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px; margin-right: 5px;"><input type="button" value="Entrar" style="height: 50px; width: 80px; color: #FFFFFF; background-color: #333333; border: 0px;"></div>
+							</div>
+						</div>
+
+						<div id="area-investidor" style="float: left; width: 100%;">
+							<h2 style="margin-bottom: 45px;">Acesso Investidor</h2>
+
+							<div class="BoxTodosMaior BoxInvestidor" onclick="javascript: btInvestidor();" style="margin-left: 285px;">
+								<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
+									<a href="#set-5" class="hi-icon hi-icon-archive cor2c3e50">Portal</a>
+									<div>Acesso Investidor</div>
 								</div>
 							</div>
 
