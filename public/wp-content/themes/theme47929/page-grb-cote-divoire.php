e871ce294b39f9383572093a511b178aebd87232
fernandofmendes <fernando.mendes@webca.com.br>
2015-09-26 07:11:36 -0300

    updates

---
+++ public/wp-content/themes/theme47929/page-grb-cote-divoire.php
@@ -0,0 +1,321 @@
+<?php
+/**
+* Template Name: Fullwidth Page
+*/
+
+get_header(); ?>
+
+<style type="text/css">
+.header {
+  height: 370px;
+}
+.title-header {
+	display: none;
+}
+</style>
+
+<div class="motopress-wrapper content-holder clearfix">
+	<div class="container">
+		<div class="row">
+			<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-wrapper-file="page-fullwidth.php" data-motopress-wrapper-type="content">
+				<div class="row">
+					<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-type="static" data-motopress-static-file="static/static-title.php">
+						<?php get_template_part("static/static-title"); ?>
+					</div>
+				</div>
+				<div id="content" class="row">
+					<div class="<?php echo cherry_get_layout_class( 'full_width_content' ); ?>" data-motopress-type="loop" data-motopress-loop-file="loop/loop-page.php">
+						
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style type="text/css">
+.title-header {
+display: none;
+}
+.content-holder {
+padding-bottom: 0px !important;
+}
+.header {
+    background-image: url('/wp-content/themes/theme47929/images/header_emp_cemosa2.jpg');
+    border-bottom: solid 5px #AB9632;
+    background-position-y: -180px !important;
+    height: 300px;
+}
+/* empresas */
+.float-empresas {
+float: left;
+width: 25%;
+text-align: center;
+margin-top: 30px;
+}
+.float-image {
+max-height: 110px;
+}
+.float-empresas-texto {
+text-align: justify;
+font-size: 12px;
+padding: 15px;
+}
+/* projetos */
+.float-projetos {
+float: left;
+width: 261px;
+text-align: center;
+margin-top: 0px;
+padding: 15px 15px 0 15px;
+}
+.float-projetos-image {
+background-size: cover;
+width: 100%;
+height: 100px;
+height: 180px;
+border: solid 5px;
+}
+.float-projetos-image:hover {
+border: solid 5px #AB9632;
+}
+.float-projetos-texto {
+text-align: justify;
+font-size: 12px;
+margin-bottom: 10px;
+}
+.float-projetos-titulo {
+font-size: 14px;
+font-weight: bold;
+text-align: left;
+margin-bottom: 5px;
+}
+/* footer */
+.footer2ul a {
+color: #FFFFFF;
+}
+.footer2ul {
+margin: 0;
+padding: 0;
+list-style-type: none;
+display: inline;
+}
+.footer2li {
+display: inline;
+}
+</style>
+
+<div style="float: left; width: 355px;">
+<img src="/wp-content/uploads/logo-png/logo-grb-cote-g.png" style="margin-left: 30px;margin-top: 10px;width: 280px;" width="280">
+</div>
+
+<div style="float: left; width: 805px;">
+
+<div><h2 style="color: #AB9632;">GRB Cotê D’Ivoire</h2></div>
+
+<div>A GRB Cotê D’Ivoire está focada em segmentos mistos voltado a obras civis, eletromecânica industriais, adequação ambiental e modernização fabril assim com obras comerciais.
+
+Trabalhamos com processos dinâmicos de modo a possibilitar trabalhos sem interrupção das atividades diárias de nossos clientes.
+
+Com experiência profissional em suas respectivas áreas de atuação, tendo como meta a satisfação dos seus clientes, seja no cumprimento dos prazos e especial observância dos padrões de qualidade, seja na observancia das normas técnicas dos serviços a que se propõem executar, assim foi constituída a GRB Cotê D’Ivoire e posteriormente a Rota Engenharia, empresas que integram o Grupo Rota do Brasil desde 2004.
+
+A GRB Cotê D’Ivoire tem como meta a conquista de novos espaços no mercado de construção nacional e internacional em suas áreas de atuação, visando assegurar através de suas competências, a confiança de seus clientes, o que é por nós considerado característica primordial para transformar um projeto em realidade sendo creditado aos nossos clientes internos e externos o sucesso por nós alcançados.</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div style="float: left; font: normal 22px Open Sans; margin-top: 30px; width: 100%; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632;margin-bottom: 15px;">Visão, missão e valores</div>
+
+<div>
+Visão:
+Ser uma empresa rentável e estratégica no mercado da Construção nacional e Internacional.
+
+Missão:
+Planejar, Gerenciar e Executar projetos de Engenharia Civil com eficiencia e eficácia gerando confiança e respeito de nossos clientes, promover a satisfação de nossos colaboradores, foco na melhoria contínua e obter rentabilidade do capital investido.
+
+Valores:
+Trabalhar com ética,qualidade,Fidelidade com os objetivos da Empresa, Lealdade, ousadia, Responsabilidade nos compromissos assumidos e transparencia nas relações com nossos clientes internos e externos
+</div>
+
+
+
+
+
+
+<div style="float: left; font: normal 22px Open Sans; margin-top: 30px; width: 100%; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632;margin-bottom: 15px;">Serviços</div>
+
+<div>
+A GRB Cotê D’Ivoire, atua para executar e construir obras e serviços tais como:
+
+Construção e Modernização de fábricas, bases para máquinas industriais, Adequação ambiental, Pisos industriais Terraplenagem, Drenagem, Escavações, Estradas, Prédios comerciais e Galpões (pequenos, médios e grandes portes), Instalações de água e esgoto, Estrutura de concreto armado, Instalações hidrosanitárias e elétricas, Impermeabilizações, pinturas industriais e comerciais, Desmobilizações de prédios e estruturas e montagens industrial.
+</div>
+
+
+
+
+
+
+
+
+
+
+<div style="float: left; font: normal 22px Open Sans; margin-top: 30px; width: 100%; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632;">Galeria</div>
+
+
+
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/290720111395.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/220720111379.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/180620111263.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/10122010946.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/290720111395.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/220720111379.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/180620111263.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/10122010946.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/290720111395.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/220720111379.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/180620111263.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+<div class="float-projetos">
+        <div style="background-image: url('/wp-content/uploads/2015/08/10122010946.jpg');" class="float-projetos-image">&nbsp;</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div style="float: left; font: normal 22px Open Sans; margin-top: 30px; width: 100%; height: 41px; padding: 10px 10px 3px;background-color: #f2f2f2;border-left: solid 10px #AB9632;color: #AB9632;">Onde estamos</div>
+
+
+
+
+
+
+<div style="float: left; width: 270px; text-align: right; margin-top: 30px;">
+<div style=" padding-right: 15px;"><b>GRB Cotê D’Ivoire</b>
+Av. Princesa Isabel, 240 - Parque Recreio
+Contagem - MG
+32110-000
+</div>
+</div>
+
+
+
+<div style="float: left; width: 899px; margin-top: 30px;">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3752.075095311047!2d-44.02189199999999!3d-19.879050700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa693fdc9c7944b%3A0x679013ccfca127f6!2sRota+Construtora!5e0!3m2!1sen!2sbr!4v1438725482248" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+
+
+
+
+
+
+</div></div>
+
+<!-- Rodape interno -->
+<a href="/fale-conosco/">
+<div style="background-image: url('/wp-content/themes/theme47929/images/header_bg_faleconosco_all.jpg');height: 200px;  background-position-y: -175px;float: left; margin-top: 40px;width: 1230px; ">
+  <div class="camera_caption fadeIn" style="position: relative; opacity: 0.8;"><div style="padding: 20px;margin: 20px;width: 115px;font-size: 16px;color: #FFFFFF;background-color: #AB9632;text-transform: uppercase;">
+                Fale conosco
+    </div></div>
+</div>
+</a>
+
+<div style="background-color: #AB9632; float: left; width: 1170px; padding: 30px; text-align: center;">
+<ul class="footer2ul">
+<li class="footer2li"><u><b><a href="/engenharia-e-construcao">Engenharia e construção</a></b></u> | </li>
+<li class="footer2li"><a href="/servicos-empresariais">Serviços empresariais</a> | </li>
+<li class="footer2li"><a href="/seguranca-patrimonial">Segurança Patrimonial</a> | </li>
+<li class="footer2li"><a href="/imobiliario">Imobiliário</a> | </li>
+<li class="footer2li"><a href="/saneamento">Saneamento</a> | </li>
+<li class="footer2li"><a href="/transporte-e-logistica">Transporte e Logística</a> | </li>
+<li class="footer2li"><a href="/negocios-internacionais">Negocios Internacionais</a> | </li>
+<li class="footer2li"><a href="/responsabilidade-social">Responsabilidade Social</a> | </li>
+<li class="footer2li"><a href="/ambiental">Ambiental</a></li>
+</ul>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
